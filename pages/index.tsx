2a9815f5129ba1d67146f857ce1f5d4f9cd89009
lucas jesus <lucas.jesus@picpay.com>
2022-12-05 18:04:42 -0300

    finishing optmization

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,96 +1,101 @@
-import { GoogleSpreadsheet } from "google-spreadsheet";
-import { sheets } from "googleapis/build/src/apis/sheets";
-import { Suspense, useState, useEffect } from "react";
-import styles from '../styles/Home.module.css'
+import { GoogleSpreadsheet, GoogleSpreadsheetRow, GoogleSpreadsheetWorksheet } from "google-spreadsheet";
+import { useState, useEffect } from "react";
+import styles from "../styles/Home.module.css";
 export default function Home() {
   const SPREADSHEET_ID = process.env.REACT_APP_SPREADSHEET_ID;
-const SHEET_ID = process.env.REACT_APP_SHEET_ID;
-const CLIENT_EMAIL = process.env.REACT_APP_GOOGLE_CLIENT_EMAIL;
-const PRIVATE_KEY = process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY;
-const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
-const [url, setUrl] = useState('')
-const [rowData, setRowData] = useState<string[]>([])
-var c = 0
-const [num, setNum] = useState(0)
-useEffect(() => {
-  c ++
-  setNum(localStorage.getItem('id') !== null ? JSON.parse(localStorage.getItem('id')) : 0)
-  if(c === 1){
-    getUser(localStorage.getItem('id') !== null ? JSON.parse(localStorage.getItem('id')) : 0)
-    document.addEventListener('keydown', (e) => {
-      if(e.key === 'ArrowRight'){
-      pJotinha('1')
-    }
-  else if(e.key === 'ArrowLeft'){pJotinha('0')}})
-  }
-}, [])
-useEffect(()=>{
-  if(num !== 0){
-    localStorage.setItem('id', num.toString())
-  }
-  
-}, [num])
-
-const getUser = async (id:number) => {
-  try{
+  const SHEET_ID = process.env.REACT_APP_SHEET_ID
+    ? process.env.REACT_APP_SHEET_ID
+    : "";
+  const CLIENT_EMAIL = process.env.REACT_APP_GOOGLE_CLIENT_EMAIL
+    ? process.env.REACT_APP_GOOGLE_CLIENT_EMAIL
+    : "";
+  const PRIVATE_KEY = process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY
+    ? process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY
+    : "";
+  const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+  const [rows, setRows] = useState<GoogleSpreadsheetRow[]>([]);
+  const [sheet, setSheet] = useState<GoogleSpreadsheetWorksheet>();
+  const [rowData, setRowData] = useState<GoogleSpreadsheetRow>();
+  const [counter, setCounter] = useState(0)
+  const [num, setNum] = useState(0);
+  const getSheet = async () => {
     await doc.useServiceAccountAuth({
       client_email: CLIENT_EMAIL,
-      private_key: PRIVATE_KEY.replace(/\\n/g, '\n'),
+      private_key: PRIVATE_KEY.replace(/\\n/g, "\n"),
+    });
+    await doc.loadInfo();
+    const sheet = doc.sheetsById[SHEET_ID];
+    sheet.loadCells()
+    const qrows = await sheet.getRows();
+    setSheet(sheet)
+    setRows(qrows)
+  };
+  useEffect(() => {
+    getSheet()
+  }, []);
+  useEffect(()=>{
+    if (!!rows && counter === 0 && rows.length > 0){
+        setCounter(5)
+        const t = localStorage.getItem("id");
+        getUser(t !== null ? JSON.parse(t) : 0);
+        document.addEventListener("keydown", (e) => {
+          if (e.key === "ArrowRight") {
+            pJotinha("1");
+          } else if (e.key === "ArrowLeft") {
+            pJotinha("0");
+          }
+        });
+        
     }
-    )
-
-    await doc.loadInfo()
-    const sheet = doc.sheetsById[SHEET_ID]
-    const rows =await sheet.getRows()
-    const row = rows[id - 2]
-    console.log(id)
-    setRowData(row);
-  }
-
-    catch (e) {
-      console.error('Error: ', e);
+  }, [rows])
+  useEffect(() => {
+    if (num !== 0) {
+      localStorage.setItem("id", num.toString());
     }
-}
+  }, [num]);
 
-const pJotinha = async (classification:string) => {
-  let stg = localStorage.getItem('id') !== null ? JSON.parse(localStorage.getItem('id')) : 0
-  try{
-    await doc.useServiceAccountAuth({
-      client_email: CLIENT_EMAIL,
-      private_key: PRIVATE_KEY.replace(/\\n/g, '\n'),
+  const getUser = async (id: number) => {
+    try {
+      const row = rows ? rows[id - 2] : rows;
+      setRowData(row);
+    } catch (e) {
+      console.error("Error: ", e);
     }
-    )
-    await doc.loadInfo()
-    const sheet = doc.sheetsById[SHEET_ID]
-    await sheet.loadCells()
-    const rCell = `E${stg}`
-    const cell = await sheet.getCellByA1(rCell)
-    cell.value = classification
-    await sheet.saveUpdatedCells()
-    setNum(stg + 1)
-    getUser(stg + 1)
-    console.log(url)
-  }
+  };
 
-  catch (e) {
-    console.error('Error: ', e);
-  }
-}
-
+  const pJotinha = async (classification: string) => {
+    const t = localStorage.getItem("id");
+    let stg = t !== null ? JSON.parse(t) : 0;
+    try {
+      if(sheet){
+        const rCell = `E${stg}`;
+        const cell =   sheet.getCellByA1(rCell);
+        cell.value = classification;
+         sheet.saveUpdatedCells();
+        setNum(stg + 1);
+        getUser(stg + 1);
+      }
+    } catch (e) {
+      console.error("Error: ", e);
+    }
+  };
 
   return (
     <div>
       <h1 className={styles.title}>eice</h1>
-      <input id='link' placeholder="Insira o Link da Planilha"></input>
-      {<p >{rowData['profile_name']}, {rowData['user_name']}, <img src={rowData['profile_image_url']} width='10%'></img></p>}
-
-      <button onClick={()=>{
-        pJotinha('0')
-      }}>Não é pjotinha</button>
-      
-      <button onClick={()=>{
-        pJotinha('1')
-      }}>É pjotinha</button>
+      <input id="link" placeholder="Insira o Link da Planilha"></input>
+      {rowData ? (
+        <p>
+          {rowData["profile_name"]}, {rowData["user_name"]},{" "}
+          <img
+            alt="imagem"
+            src={rowData["profile_image_url"]}
+            width="10%"
+          ></img>
+        </p>
+      ) : (
+        <></>
+      )}
     </div>
-  )
+  );
 }
