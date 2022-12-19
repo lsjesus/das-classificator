ca3b01c0b5574ab6f6129502ab53ba42c919dd97
matheusHCDdias <116729084+matheusHCDdias@users.noreply.github.com>
2022-12-19 11:13:01 -0300

    first commit to change button

---
+++ components/SelectButton/SelectButton.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect, useRef } from "react";
+import { SelectDiv } from './style';
+import { Header } from "../../components";
+
+
+
+
+export function Select(){
+    const inputReference = useRef<HTMLInputElement>(null)
+
+    const getButton = async () => {
+        if(inputReference.current)
+            var buttonPf = inputReference.current.value[0]
+            var buttonPj = inputReference.current?.value[1]
+    }
+
+    return(
+        <>
+        <Header></Header>
+        <form action="./" onSubmit={(e) => { getButton() }}>
+        <SelectDiv>
+            <div className="boxFormat">
+                <p className="titleFormat"></p>
+                <label className="subTitleFormat" htmlFor="button">Insira a seguir o botões para avaliação</label>
+                <input id="buttonPF" className="inputFormat" type="text" ref={inputReference} placeholder="Botão PF"/>
+                <input id="buttonPJ" className="inputFormat" type="text" ref={inputReference} placeholder="Botão PJ"/>
+                <button type="submit" className="buttonFormat">Confirmar</button>
+            </div>
+        </SelectDiv>
+        </form>
+        </>
+    )
+}
